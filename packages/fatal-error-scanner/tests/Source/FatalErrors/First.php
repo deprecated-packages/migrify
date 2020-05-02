7f2ecb87a5adf18359b6e691d79507fd5ff05918
TomasVotruba <tomas.vot@gmail.com>
2020-05-03 01:10:27 +0200

    add workflow test for fatal error scan

---
+++ packages/fatal-error-scanner/tests/Source/FatalErrors/First.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\FatalErrorScanner\Tests\Source\FatalErrors;
+
+final class First extends AbstractTest
+{
+    public function remove(): void
+    {
+    }
+}
---
+++ packages/fatal-error-scanner/tests/Source/FatalErrors/Second.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\FatalErrorScanner\Tests\Source\FatalErrors;
+
+final class Second extends AbstractTest
+{
+    public function remove(): void
+    {
+    }
+}
