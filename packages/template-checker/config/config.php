ded27e1fc413eacae7a5cde115cfb37f398e55a2
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 11:56:05 +0200

    add fqn function names

--- packages/template-checker/config/config.php
+++ packages/template-checker/config/config.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Symfony\Component\DependencyInjection\Loader\Configurator;
-
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
 use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
