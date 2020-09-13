1888bef0008d53cbb94ab2601e35d4bdec808647
Tomas Votruba <tomas.vot@gmail.com>
2020-09-13 21:02:25 +0200

    skip DateTime and Strings class (#164)

--- packages/template-checker/src/StaticCallWithFilterReplacer.php
+++ packages/template-checker/src/StaticCallWithFilterReplacer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Migrify\TemplateChecker;
 
+use Nette\Utils\DateTime;
 use Nette\Utils\Strings;
 use Symfony\Component\Finder\SplFileInfo;
 
@@ -24,6 +25,11 @@ final class StaticCallWithFilterReplacer
         $contents = $fileInfo->getContents();
 
         return Strings::replace($contents, self::STATIC_CALL_PATTERN, static function (array $match) {
+            if (in_array($match['class'], [Strings::class, DateTime::class], true)) {
+                // no change
+                return $match[0];
+            }
+
             return $match['method'] . '(' . $match['arguments'] . ')';
         });
     }
