f92bcb214b5346d86ee814e9e4182d49c2398bdb
TomasVotruba <tomas.vot@gmail.com>
2020-09-18 13:40:09 +0200

    update paths

--- packages/easy-ci/src/Sonar/SonarConfigGenerator.php
+++ packages/easy-ci/src/Sonar/SonarConfigGenerator.php
@@ -7,13 +7,21 @@ namespace Migrify\EasyCI\Sonar;
 use Migrify\EasyCI\Finder\SrcTestsDirectoriesFinder;
 use Migrify\EasyCI\ValueObject\Option;
 use Migrify\EasyCI\ValueObject\SonarConfigKey;
+use Nette\Utils\Strings;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 /**
  * @see \Migrify\EasyCI\Tests\Sonar\SonarConfigGenerator\SonarConfigGeneratorTest
  */
 final class SonarConfigGenerator
 {
+    /**
+     * @see https://regex101.com/r/YbxRKD/1
+     * @var string
+     */
+    private const SONAR_KEY_VALUE_REGEX = '#(?<key>sonar\..*?)=(?<value>.*?)$#m';
+
     /**
      * @var SrcTestsDirectoriesFinder
      */
@@ -30,6 +38,30 @@ final class SonarConfigGenerator
     ) {
         $this->srcTestsDirectoriesFinder = $srcTestsDirectoriesFinder;
         $this->parameterProvider = $parameterProvider;
+    }
+
+    /**
+     * @param string[] $projectDirectories
+     */
+    public function generateWithOriginalFile(array $projectDirectories, SmartFileInfo $originalFileInfo): string
+    {
+        $fileContent = $this->generate($projectDirectories);
+
+        // to keep distance
+        $fileContent .= PHP_EOL;
+
+        $sonarKeyValues = $this->resolveSonarKeyValues($originalFileInfo);
+        foreach ($sonarKeyValues as $key => $value) {
+            // prevent overriding generated keys
+            $keyPattern = '#^' . preg_quote($key) . '=(.*?)$#ms';
+            if (Strings::match($fileContent, $keyPattern)) {
+                continue;
+            }
+
+            $fileContent = $this->appendKeyLine($fileContent, $key, $value);
+        }
+
+        return rtrim($fileContent) . PHP_EOL;
     }
 
     /**
@@ -74,5 +106,24 @@ final class SonarConfigGenerator
         $fileContent .= PHP_EOL . PHP_EOL;
 
         return $fileContent;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function resolveSonarKeyValues(SmartFileInfo $originalFileInfo): array
+    {
+        $sonarKeyValueMatches = Strings::matchAll($originalFileInfo->getContents(), self::SONAR_KEY_VALUE_REGEX);
+
+        $originaSonarKeyValues = [];
+
+        foreach ($sonarKeyValueMatches as $sonarKeyValueMatch) {
+            $key = (string) $sonarKeyValueMatch['key'];
+            $value = (string) $sonarKeyValueMatch['value'];
+
+            $originaSonarKeyValues[$key] = $value;
+        }
+
+        return $originaSonarKeyValues;
     }
 }
--- packages/easy-ci/tests/Sonar/SonarConfigGenerator/SonarConfigGeneratorTest.php
+++ packages/easy-ci/tests/Sonar/SonarConfigGenerator/SonarConfigGeneratorTest.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Migrify\EasyCI\Tests\Sonar\SonarConfigGenerator;
 
+use Iterator;
 use Migrify\EasyCI\HttpKernel\EasyCIKernel;
 use Migrify\EasyCI\Sonar\SonarConfigGenerator;
 use Migrify\EasyCI\ValueObject\Option;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class SonarConfigGeneratorTest extends AbstractKernelTestCase
 {
@@ -32,5 +34,38 @@ final class SonarConfigGeneratorTest extends AbstractKernelTestCase
     {
         $sonarConfigContent = $this->sonarConfigGenerator->generate([__DIR__ . '/Fixture']);
         $this->assertStringEqualsFile(__DIR__ . '/Fixture/expected_config.txt', $sonarConfigContent);
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function testWithOriginalFile(
+        string $fixtureDirectory,
+        string $originalFilePath,
+        string $expectedSonartConfig
+    ): void {
+        $originalFileInfo = new SmartFileInfo($originalFilePath);
+
+        $sonarConfigContent = $this->sonarConfigGenerator->generateWithOriginalFile(
+            [$fixtureDirectory],
+            $originalFileInfo
+        );
+
+        $this->assertStringEqualsFile($expectedSonartConfig, $sonarConfigContent);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [
+            __DIR__ . '/Fixture',
+            __DIR__ . '/Fixture/original_config.txt',
+            __DIR__ . '/Fixture/expected_modified_original_config.txt',
+        ];
+
+        yield [
+            __DIR__ . '/Fixture',
+            __DIR__ . '/Fixture/original_config_with_paths.txt',
+            __DIR__ . '/Fixture/expected_modified_original_config.txt',
+        ];
     }
 }
