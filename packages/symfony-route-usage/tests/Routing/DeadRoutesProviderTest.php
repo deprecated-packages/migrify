49a310490ebcf4c987bffefb83f745a463f0f62d
Bartosz Błyszcz <>
2020-10-21 11:53:25 +0200

    symfony_route_translate

--- packages/symfony-route-usage/tests/Routing/DeadRoutesProviderTest.php
+++ packages/symfony-route-usage/tests/Routing/DeadRoutesProviderTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Migrify\SymfonyRouteUsage\Tests\Routing;
 
+use Migrify\SymfonyRouteUsage\Entity\RouteVisit;
+use Migrify\SymfonyRouteUsage\EntityRepository\RouteVisitRepository;
 use Migrify\SymfonyRouteUsage\Routing\DeadRoutesProvider;
 use Migrify\SymfonyRouteUsage\Tests\Helper\DatabaseLoaderHelper;
 use Migrify\SymfonyRouteUsage\Tests\HttpKernel\SymfonyRouteUsageKernel;
@@ -16,6 +18,11 @@ final class DeadRoutesProviderTest extends AbstractKernelTestCase
      */
     private $deadRoutesProvider;
 
+    /**
+     * @var RouteVisitRepository
+     */
+    private $routeVisitRepository;
+
     protected function setUp(): void
     {
         $this->bootKernel(SymfonyRouteUsageKernel::class);
@@ -23,12 +30,15 @@ final class DeadRoutesProviderTest extends AbstractKernelTestCase
         $databaseLoaderHelper = new DatabaseLoaderHelper(self::$container);
         $databaseLoaderHelper->disableDoctrineLogger();
         $databaseLoaderHelper->createDatabase();
-
         $this->deadRoutesProvider = self::$container->get(DeadRoutesProvider::class);
+        $this->routeVisitRepository = self::$container->get(RouteVisitRepository::class);
     }
 
     public function test(): void
     {
+        $routeVisit = new RouteVisit('acme_privacy', "{'route':'params'}", 'SomeController', 'some_hash');
+        $this->routeVisitRepository->save($routeVisit);
+
         $deadRoutes = $this->deadRoutesProvider->provide();
         $this->assertCount(0, $deadRoutes);
 
